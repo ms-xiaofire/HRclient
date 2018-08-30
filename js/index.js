dcc02b9945af7fa59c2183ea73e2165b7aa5e039
ms-xiaofire <916646626@qq.com>
2018-08-30 17:30:13 +0800

    20180830-1730

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// 弹窗
+//登录
+$('#login').click(function () {
+    window.location.href = 'dealMain.html';
+});
+//找回密码
+$('#submit-pwd').click(function () {
+    $('#dim').addClass('dim');
+    $('.password').show();
+});
+//注册
+$('#submit-reg').click(function () {
+    $('#dim').addClass('dim');
+    $('.reg').show();
+});
